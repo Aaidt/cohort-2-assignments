50745a439cada3560751e81c1f1c46d8968f9f03
Aaidt <aadits2004@gmail.com>
2025-01-06 22:40:00 +0530

    02 and 03 class solutions

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -10,11 +10,26 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
-
 let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+
+app.use(function(req, res, next) {
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("no entry...");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
