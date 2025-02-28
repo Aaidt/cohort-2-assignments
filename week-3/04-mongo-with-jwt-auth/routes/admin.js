0209a0a0f834fc741d029fc91da6e6a28d23a7a7
Aaidt <aadits2004@gmail.com>
2025-02-28 21:37:38 +0530

    added sol2

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,34 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../index");
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const { username, password } = req.body;
+    const user = await User.findOne({
+        username,
+        password
+    });
+
+    if(user){
+        jwt.sign({
+            username
+        }, JWT_SECRET);
+        res.json({
+            token
+        })
+        
+    }else{
+        res.json({
+            msg: "User doesnt exist."
+        })
+    } 
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
